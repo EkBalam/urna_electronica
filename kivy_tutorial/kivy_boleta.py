10def62c8d48b7c1c14f109120a961c58e82d65c
Adan <adan@latailabs.com>
2022-08-27 19:09:53 -0500

    pantalla confirmación

--- kivy_tutorial/kivy_boleta.py
+++ kivy_tutorial/kivy_boleta.py
@@ -1,12 +1,13 @@
 
 from cgitb import text
-from tkinter import Button, font
-from turtle import pos
+from turtle import color
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.image import Image
+from kivy.uix.modalview import ModalView
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.graphics import Color, RoundedRectangle
 from kivy.core.window import Window
@@ -56,8 +57,10 @@ class OpcionBoleta(ButtonBehavior, BoxLayout):
     def __init__(self, image_path, propietario, suplente, hipocoristico, color = (0.2, 0.5, 0.5, 0.5), text_color = (1, 1, 1, 1), **kwargs):
         super(OpcionBoleta, self).__init__(**kwargs)        
         self.padding = [50, 50]
-        self.cols = 2 
+        #self.cols = 2 
         self.propietario = propietario        
+        self.hipocoristico = hipocoristico
+        self.img_path = image_path
         self.size = (500, 150)
         self.rect_size = (self.size[0]-10, self.size[1]-10)
         
@@ -86,6 +89,8 @@ class OpcionBoleta(ButtonBehavior, BoxLayout):
         self.rect.size = self.rect_size
 
     def on_press(self):
+        view = AceptarVoto(self.propietario, self.hipocoristico, self.img_path)
+        view.open()
         print(self.propietario)
         return super().on_press()
 
@@ -137,6 +142,48 @@ class BoletaScreen(GridLayout):
         self.add_widget(OpcionBoleta('images/sombrerito.webp', 'Juan Jimenez', 
         'Pedro García', 'Partido Sombrero', color=amarillo, text_color=negro))        
 
+
+class AceptarVoto(ModalView):
+
+    def __init__(self, propietario, partido, img_path,  **kwargs):
+        super(AceptarVoto, self).__init__(**kwargs)
+        self.size_hint = (None, None)
+        self.size = (600, 600)
+        self.auto_dismiss = False
+        
+        with self.canvas:            
+            Color(rgba = (1.00000, 0.90196, 0.80000, 1))       
+            self.rect = RoundedRectangle(pos=self.pos, size=self.size, radius = [(20, 20), (20, 20)])
+
+        ople_color = (0.90196, 0.00000, 0.45098, 1)
+
+        boxL = BoxLayout(orientation='vertical', padding=(20))
+        boxL.add_widget(Label(text='Está es su elección de voto\n presione ACEPTAR para registrarlo\n o CANCELAR para regresar', 
+                                font_size = 32, color = (0,0,0,1)))
+        
+        boxL.add_widget(Label(text=propietario, font_size = 36, color = ople_color))
+        self.add_widget(Image(source=img_path, size_hint =(0.2, 0.5)))                     
+        boxL.add_widget(Label(text=partido, font_size = 36, color = ople_color))
+
+
+        gl = GridLayout(cols=2, padding=(24))
+        self.button_aceptar = Button(text='ACEPTAR')
+        gl.add_widget(self.button_aceptar)
+
+        self.button_cancel = Button(text='CANCELAR')
+        gl.add_widget(self.button_cancel)
+        
+        boxL.add_widget(gl)
+        self.add_widget(boxL)        
+
+        self.button_cancel.bind(on_press = self.dismiss)
+
+        self.bind(pos=self.update_rect)
+        self.bind(size=self.update_rect)
+
+    def update_rect(self, *args):
+        self.rect.pos = self.pos
+        self.rect.size = self.size
 
 class MyApp(App):
     def build(self):
